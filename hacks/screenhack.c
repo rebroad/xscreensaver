2c570c26d0c6488244d93ef371c766e9024c0941
R E Broadley <rebroad@users.noreply.github.com>
2025-02-14 09:58:06 +0000

    Remove duplicate SDL_Init

--- hacks/screenhack.c
+++ hacks/screenhack.c
@@ -809,6 +809,11 @@ int main (int argc, char **argv)
 	return 1;
   }
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
   SDL_Window *window = SDL_CreateWindow(
     ft->progclass, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 	1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
@@ -819,7 +824,7 @@ int main (int argc, char **argv)
 	return 1;
   }
 
-  SDL_GLContext gl_context = SDL_GL_CreateContent(window);
+  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
   if (!gl_context) {
 	fprintf(stderr, "OpenGL context creation failed: %s\n", SDL_GetError());
 	SDL_DestroyWindow(window);
