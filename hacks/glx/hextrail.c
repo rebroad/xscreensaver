d1f352c24f60c2b72688bc066ada1f2856e055a1
R E Broadley <rebroad@users.noreply.github.com>
2025-02-14 09:58:06 +0000

    Remove duplicate SDL_Init

--- hacks/glx/hextrail.c
+++ hacks/glx/hextrail.c
@@ -41,37 +41,6 @@
 #ifdef USE_SDL
 SDL_Window* window;
 SDL_GLContext glContext;
-
-static Bool init_sdl(ModeInfo *mi) {
-  hextrail_configuration *bp = &bps[MI_SCREEN(mi)];
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-	return False;
-  }
-
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-  bp->window = SDL_CreateWindow("HexTrail",
-    SDL_WINDOWPOS_CENTERED, SDL_WINDOWSPOS_CENTERED,
-	MI_WIDTH(mi), MI_HEIGHT(mi), SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-
-  if (!bp->window) {
-	return False;
-  }
-
-  bp->glContext = SDL_GL_CreateContext(bp->window);
-  if (!bp->gl_context) {
-	SDL_DestroyWindow(bp->window);
-	return False;
-  }
-
-  SDL_GL_SetSwapInterval(1); // Enable vsync
-
-  return True;
-}
 #endif
 
 #define DEF_SPIN        "True"
