e041271f73c632cc9676e24be42fcfbf14aacaee
R E Broadley <rebroad@users.noreply.github.com>
2025-02-14 16:28:56 +0000

    recuce comments to reduce Claude tokens

--- hacks/xlockmoreI.h
+++ hacks/xlockmoreI.h
@@ -1,15 +1,5 @@
 /* xlockmore.h --- xscreensaver compatibility layer for xlockmore modules.
  * xscreensaver, Copyright (c) 1997-2021 Jamie Zawinski <jwz@jwz.org>
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation.  No representations are made about the suitability of this
- * software for any purpose.  It is provided "as is" without express or 
- * implied warranty.
- *
- * See xlockmore.h and xlockmore.c.
  */
 
 #ifndef __XLOCKMORE_INTERNAL_H__
