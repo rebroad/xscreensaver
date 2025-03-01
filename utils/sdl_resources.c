8876bf36923919f76934769239855ba243dd919c
R E Broadley <rebroad@users.noreply.github.com>
2025-03-01 11:27:21 +0000

    sdl_resources - just what we need for now

--- utils/sdl_resources.c
+++ utils/sdl_resources.c
@@ -1,81 +1,39 @@
-/* resources_common.c */
-#include "resources_common.h"
-#include <string.h>
-#include <stdlib.h>
+#ifdef USE_SDL
+#include "screenhackI.h"
+static char *resource_db[1024]; // Simple static store
+static int resource_count = 0;
 
-ResourceManager *resource_manager_init(int argc, char **argv, const XrmOptionDescRec *options, const char * const *defaults) {
-    ResourceManager *rm = calloc(1, sizeof(ResourceManager));
-    rm->entries = calloc(argc + 100, sizeof(ResourceEntry)); // Oversize for defaults
-    
-    // Parse command-line options
-    int entry_count = 0;
-    for (int i = 1; i < argc && options; i++) {
-        for (int j = 0; options[j].option; j++) {
-            if (strcmp(argv[i], options[j].option) == 0) {
-                rm->entries[entry_count].name = strdup(options[j].specifier + 1); // Skip '.'
-                if (options[j].argKind == XrmoptionNoArg) {
-                    rm->entries[entry_count].value = strdup("True");
-                } else if (i + 1 < argc && argv[i + 1][0] != '-') {
-                    rm->entries[entry_count].value = strdup(argv[++i]);
-                }
-                entry_count++;
+void sdl_init_resources(int argc, char **argv, const XrmOptionDescRec *opts, const char * const *defaults) {
+    int i, j;
+    // Copy defaults into resource_db
+    for (i = 0; defaults[i]; i++) resource_db[resource_count++] = strdup(defaults[i]);
+    // Parse argv using opts
+    for (i = 1; i < argc; i++) {
+        for (j = 0; opts[j].option; j++) {
+            if (!strcmp(argv[i], opts[j].option)) {
+                char *key = opts[j].specifier + 1; // Skip '.'
+                char *value = (opts[j].argKind == XrmoptionNoArg) ? "True" :
+                              (i + 1 < argc && argv[i + 1][0] != '-') ? argv[++i] : "";
+                char *entry = malloc(strlen(key) + strlen(value) + 3);
+                sprintf(entry, "%s: %s", key, value);
+                resource_db[resource_count++] = entry;
                 break;
             }
         }
     }
-    
-    // Add defaults
-    for (int i = 0; defaults && defaults[i]; i++) {
-        char *name = strdup(defaults[i]);
-        char *value = strchr(name, ':');
-        if (value) {
-            *value++ = 0;
-            while (*value == ' ' || *value == '\t') value++;
-            rm->entries[entry_count].name = name;
-            rm->entries[entry_count].value = strdup(value);
-            entry_count++;
-        } else {
-            free(name);
+}
+
+Bool get_boolean_resource(void *unused, char *name, char *class) {
+    for (int i = 0; i < resource_count; i++) {
+        char *s = resource_db[i], *colon = strchr(s, ':');
+        if (colon && !strncmp(s, name, colon - s)) {
+            char *val = colon + 1;
+            while (*val == ' ' || *val == '\t') val++;
+            return !strcasecmp(val, "True") || !strcasecmp(val, "on") || !strcasecmp(val, "yes");
         }
     }
-    
-    rm->count = entry_count;
-    return rm;
+    return False; // Default
 }
 
-void resource_manager_free(ResourceManager *rm) {
-    for (int i = 0; i < rm->count; i++) {
-        free(rm->entries[i].name);
-        free(rm->entries[i].value);
-    }
-    free(rm->entries);
-    free(rm);
-}
-
-static const char *find_value(ResourceManager *rm, const char *name) {
-    for (int i = 0; i < rm->count; i++) {
-        if (strcmp(rm->entries[i].name, name) == 0) return rm->entries[i].value;
-    }
-    return NULL;
-}
-
-Bool resource_get_boolean(ResourceManager *rm, const char *name, Bool default_value) {
-    const char *val = find_value(rm, name);
-    if (!val) return default_value;
-    return (!strcmp(val, "True") || !strcmp(val, "on") || !strcmp(val, "yes"));
-}
-
-float resource_get_float(ResourceManager *rm, const char *name, float default_value) {
-    const char *val = find_value(rm, name);
-    return val ? atof(val) : default_value;
-}
-
-int resource_get_int(ResourceManager *rm, const char *name, int default_value) {
-    const char *val = find_value(rm, name);
-    return val ? atoi(val) : default_value;
-}
-
-char *resource_get_string(ResourceManager *rm, const char *name, const char *default_value) {
-    const char *val = find_value(rm, name);
-    return val ? strdup(val) : (default_value ? strdup(default_value) : NULL);
-}
+// Similar implementations for get_string_resource, get_integer_resource, etc.
+#endif
