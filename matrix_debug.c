5d93c5fc617581b802fd69229d752e7649d4b3c9
R E Broadley <rebroad@users.noreply.github.com>
2025-08-08 21:53:11 +0100

    matrix_debug wrap the OpenGL/WebGL wrapper!

--- matrix_debug.c
+++ matrix_debug.c
@@ -84,9 +84,11 @@ static void (*real_glScalef)(GLfloat, GLfloat, GLfloat) = NULL;
 static void (*real_glPushMatrix)(void) = NULL;
 static void (*real_glPopMatrix)(void) = NULL;
 static void (*real_glMultMatrixf)(const GLfloat*) = NULL;
+static void (*real_glMultMatrixd)(const GLdouble*) = NULL;
 static void (*real_glViewport)(GLint, GLint, GLsizei, GLsizei) = NULL;
 static void (*real_gluPerspective)(GLdouble, GLdouble, GLdouble, GLdouble) = NULL;
 static void (*real_gluLookAt)(GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble) = NULL;
+static void (*real_glTranslated)(GLdouble, GLdouble, GLdouble) = NULL;
 
 // Debug logging function
 void matrix_debug_log(const char* format, ...) {
@@ -198,23 +200,28 @@ void debug_current_matrix_state(void) {
 // Initialize function pointers (call this once)
 void init_matrix_debug_functions(void) {
     #ifdef WEB_BUILD
-    // For WebGL builds, the functions are already available from xscreensaver_web.c
-    // We just need to assign them to our function pointers
-    real_glMatrixMode = glMatrixMode;
-    real_glLoadIdentity = glLoadIdentity;
-    real_glOrtho = glOrtho;
-    real_glFrustum = glFrustum;
-    real_glTranslatef = glTranslatef;
-    real_glRotatef = glRotatef;
-    real_glScalef = glScalef;
-    real_glPushMatrix = glPushMatrix;
-    real_glPopMatrix = glPopMatrix;
-    real_glMultMatrixf = glMultMatrixf;
-    real_glViewport = glViewport;
-    real_gluPerspective = gluPerspective;
-    real_gluLookAt = gluLookAt;
+    // For WebGL builds, we need to get function pointers to the actual xscreensaver_web functions
+    // We'll use dlsym to get the real function addresses, avoiding the macro redirection
 
-    matrix_debug_log("Matrix debug: Successfully initialized WebGL function pointers\n");
+    // Get function pointers to the actual xscreensaver_web functions
+    // These are the real implementations, not our debug wrappers
+    real_glMatrixMode = (typeof(real_glMatrixMode))dlsym(RTLD_DEFAULT, "glMatrixMode");
+    real_glLoadIdentity = (typeof(real_glLoadIdentity))dlsym(RTLD_DEFAULT, "glLoadIdentity");
+    real_glOrtho = (typeof(real_glOrtho))dlsym(RTLD_DEFAULT, "glOrtho");
+    real_glFrustum = (typeof(real_glFrustum))dlsym(RTLD_DEFAULT, "glFrustum");
+    real_glTranslatef = (typeof(real_glTranslatef))dlsym(RTLD_DEFAULT, "glTranslatef");
+    real_glRotatef = (typeof(real_glRotatef))dlsym(RTLD_DEFAULT, "glRotatef");
+    real_glScalef = (typeof(real_glScalef))dlsym(RTLD_DEFAULT, "glScalef");
+    real_glPushMatrix = (typeof(real_glPushMatrix))dlsym(RTLD_DEFAULT, "glPushMatrix");
+    real_glPopMatrix = (typeof(real_glPopMatrix))dlsym(RTLD_DEFAULT, "glPopMatrix");
+    real_glMultMatrixf = (typeof(real_glMultMatrixf))dlsym(RTLD_DEFAULT, "glMultMatrixf");
+    real_glMultMatrixd = (typeof(real_glMultMatrixd))dlsym(RTLD_DEFAULT, "glMultMatrixd");
+    real_glViewport = (typeof(real_glViewport))dlsym(RTLD_DEFAULT, "glViewport");
+    real_gluPerspective = (typeof(real_gluPerspective))dlsym(RTLD_DEFAULT, "gluPerspective");
+    real_gluLookAt = (typeof(real_gluLookAt))dlsym(RTLD_DEFAULT, "gluLookAt");
+    real_glTranslated = (typeof(real_glTranslated))dlsym(RTLD_DEFAULT, "glTranslated");
+
+    matrix_debug_log("Matrix debug: WebGL build - initialized function pointers to xscreensaver_web functions\n");
     #else
     // For now, we'll use dlsym to get the real functions
     // This requires linking with -ldl
@@ -281,15 +288,10 @@ void debug_glMatrixMode(GLenum mode) {
     // Log the call and call real function (if available)
     matrix_debug_log("glMatrixMode(%d)\n", mode);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glMatrixMode(mode);
-    #else
-        // Native version: call real OpenGL function
-        if (real_glMatrixMode) {
-            real_glMatrixMode(mode);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glMatrixMode) {
+        real_glMatrixMode(mode);
+    }
 }
 
 void debug_glLoadIdentity(void) {
@@ -297,15 +299,10 @@ void debug_glLoadIdentity(void) {
 
     matrix_debug_log("glLoadIdentity()\n");
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glLoadIdentity();
-    #else
-        // Native version: call real OpenGL function
-        if (real_glLoadIdentity) {
-            real_glLoadIdentity();
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glLoadIdentity) {
+        real_glLoadIdentity();
+    }
 }
 
 void debug_glOrtho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near_val, GLdouble far_val) {
@@ -313,15 +310,10 @@ void debug_glOrtho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top,
 
     matrix_debug_log("glOrtho(%f, %f, %f, %f, %f, %f) - Matrix will be modified\n", left, right, bottom, top, near_val, far_val);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glOrtho(left, right, bottom, top, near_val, far_val);
-    #else
-        // Native version: call real OpenGL function
-        if (real_glOrtho) {
-            real_glOrtho(left, right, bottom, top, near_val, far_val);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glOrtho) {
+        real_glOrtho(left, right, bottom, top, near_val, far_val);
+    }
 }
 
 void debug_glFrustum(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble near_val, GLdouble far_val) {
@@ -329,15 +321,10 @@ void debug_glFrustum(GLdouble left, GLdouble right, GLdouble bottom, GLdouble to
 
     matrix_debug_log("glFrustum(%f, %f, %f, %f, %f, %f) - Matrix will be modified\n", left, right, bottom, top, near_val, far_val);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glFrustum(left, right, bottom, top, near_val, far_val);
-    #else
-        // Native version: call real OpenGL function
-        if (real_glFrustum) {
-            real_glFrustum(left, right, bottom, top, near_val, far_val);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glFrustum) {
+        real_glFrustum(left, right, bottom, top, near_val, far_val);
+    }
 }
 
 void debug_glTranslatef(GLfloat x, GLfloat y, GLfloat z) {
@@ -345,16 +332,10 @@ void debug_glTranslatef(GLfloat x, GLfloat y, GLfloat z) {
 
     matrix_debug_log("glTranslatef(%f, %f, %f) - Matrix will be modified\n", x, y, z);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glTranslatef(x, y, z);
-        // Note: The WebGL wrapper will call debug_matrix("Before Translate") and debug_matrix("After Translate")
-    #else
-        // Native version: call real OpenGL function
-        if (real_glTranslatef) {
-            real_glTranslatef(x, y, z);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glTranslatef) {
+        real_glTranslatef(x, y, z);
+    }
 }
 
 void debug_glRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z) {
@@ -362,16 +343,10 @@ void debug_glRotatef(GLfloat angle, GLfloat x, GLfloat y, GLfloat z) {
 
     matrix_debug_log("glRotatef(%f, %f, %f, %f) - Matrix will be modified\n", angle, x, y, z);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glRotatef(angle, x, y, z);
-        // Note: The WebGL wrapper will call debug_matrix("Before Rotate") and debug_matrix("After Rotate")
-    #else
-        // Native version: call real OpenGL function
-        if (real_glRotatef) {
-            real_glRotatef(angle, x, y, z);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glRotatef) {
+        real_glRotatef(angle, x, y, z);
+    }
 }
 
 void debug_glScalef(GLfloat x, GLfloat y, GLfloat z) {
@@ -379,15 +354,10 @@ void debug_glScalef(GLfloat x, GLfloat y, GLfloat z) {
 
     matrix_debug_log("glScalef(%f, %f, %f)\n", x, y, z);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glScalef(x, y, z);
-    #else
-        // Native version: call real OpenGL function
-        if (real_glScalef) {
-            real_glScalef(x, y, z);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glScalef) {
+        real_glScalef(x, y, z);
+    }
 }
 
 void debug_glPushMatrix(void) {
@@ -395,16 +365,10 @@ void debug_glPushMatrix(void) {
 
     matrix_debug_log("glPushMatrix() - Stack depth will increase\n");
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glPushMatrix();
-        // Note: The WebGL wrapper will call its own debug_matrix() to show the stack state
-    #else
-        // Native version: call real OpenGL function
-        if (real_glPushMatrix) {
-            real_glPushMatrix();
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glPushMatrix) {
+        real_glPushMatrix();
+    }
 }
 
 void debug_glPopMatrix(void) {
@@ -412,16 +376,10 @@ void debug_glPopMatrix(void) {
 
     matrix_debug_log("glPopMatrix() - Stack depth will decrease\n");
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glPopMatrix();
-        // Note: The WebGL wrapper will call its own debug_matrix() to show the stack state
-    #else
-        // Native version: call real OpenGL function
-        if (real_glPopMatrix) {
-            real_glPopMatrix();
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glPopMatrix) {
+        real_glPopMatrix();
+    }
 }
 
 void debug_glMultMatrixf(const GLfloat* m) {
@@ -431,59 +389,29 @@ void debug_glMultMatrixf(const GLfloat* m) {
                      m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7],
                      m[8], m[9], m[10], m[11], m[12], m[13], m[14], m[15]);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glMultMatrixf(m);
-    #else
-        // Native version: call real OpenGL function
-        if (real_glMultMatrixf) {
-            real_glMultMatrixf(m);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glMultMatrixf) {
+        real_glMultMatrixf(m);
+    }
 }
 
 // Additional functions used by hextrail
 void debug_glViewport(GLint x, GLint y, GLsizei width, GLsizei height) {
     matrix_debug_log("glViewport(%d, %d, %d, %d)\n", x, y, width, height);
 
-    #ifdef WEB_BUILD
-        // WebGL version: call the real web wrapper function
-        glViewport(x, y, width, height);
-    #else
-        // For native builds, we need to get the real function pointer
-        static void (*real_glViewport)(GLint, GLint, GLsizei, GLsizei) = NULL;
-        if (!real_glViewport) {
-            #ifdef __linux__
-            #pragma GCC diagnostic push
-            #pragma GCC diagnostic ignored "-Wpedantic"
-            real_glViewport = (typeof(real_glViewport))dlsym(RTLD_NEXT, "glViewport");
-            if (!real_glViewport) {
-                real_glViewport = (typeof(real_glViewport))dlsym(RTLD_DEFAULT, "glViewport");
-            }
-            #pragma GCC diagnostic pop
-            #endif
-        }
-
-        if (real_glViewport) {
-            real_glViewport(x, y, width, height);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glViewport) {
+        real_glViewport(x, y, width, height);
+    }
 }
 
 void debug_gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar) {
     matrix_debug_log("gluPerspective(%f, %f, %f, %f)\n", fovy, aspect, zNear, zFar);
 
-    #ifdef WEB_BUILD
-        // For WebGL, call the xscreensaver_web wrapper function directly
-        gluPerspective(fovy, aspect, zNear, zFar);
-    #else
-        // For native, use function pointer
-        static void (*real_gluPerspective)(GLdouble, GLdouble, GLdouble, GLdouble) = NULL;
-        GET_FUNC_PTR(real_gluPerspective, "gluPerspective");
-        if (real_gluPerspective) {
-            real_gluPerspective(fovy, aspect, zNear, zFar);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_gluPerspective) {
+        real_gluPerspective(fovy, aspect, zNear, zFar);
+    }
 }
 
 void debug_gluLookAt(GLdouble eyex, GLdouble eyey, GLdouble eyez,
@@ -492,17 +420,10 @@ void debug_gluLookAt(GLdouble eyex, GLdouble eyey, GLdouble eyez,
     matrix_debug_log("gluLookAt(eye=(%f, %f, %f), center=(%f, %f, %f), up=(%f, %f, %f))\n",
                      eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz);
 
-    #ifdef WEB_BUILD
-        // For WebGL, call the xscreensaver_web wrapper function directly
-        gluLookAt(eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz);
-    #else
-        // For native, use function pointer
-        static void (*real_gluLookAt)(GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble, GLdouble) = NULL;
-        GET_FUNC_PTR(real_gluLookAt, "gluLookAt");
-        if (real_gluLookAt) {
-            real_gluLookAt(eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_gluLookAt) {
+        real_gluLookAt(eyex, eyey, eyez, centerx, centery, centerz, upx, upy, upz);
+    }
 }
 
 // Additional debug functions for both native and web
@@ -511,33 +432,19 @@ void debug_glMultMatrixd(const GLdouble* m) {
                      m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7],
                      m[8], m[9], m[10], m[11], m[12], m[13], m[14], m[15]);
 
-    #ifdef WEB_BUILD
-        // For WebGL, call the xscreensaver_web wrapper function directly
-        glMultMatrixd(m);
-    #else
-        // For native, use function pointer
-        static void (*real_glMultMatrixd)(const GLdouble*) = NULL;
-        GET_FUNC_PTR(real_glMultMatrixd, "glMultMatrixd");
-        if (real_glMultMatrixd) {
-            real_glMultMatrixd(m);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glMultMatrixd) {
+        real_glMultMatrixd(m);
+    }
 }
 
 void debug_glTranslated(GLdouble x, GLdouble y, GLdouble z) {
     matrix_debug_log("glTranslated(%f, %f, %f)\n", x, y, z);
 
-    #ifdef WEB_BUILD
-        // For WebGL, call the xscreensaver_web wrapper function directly
-        glTranslated(x, y, z);
-    #else
-        // For native, use function pointer
-        static void (*real_glTranslated)(GLdouble, GLdouble, GLdouble) = NULL;
-        GET_FUNC_PTR(real_glTranslated, "glTranslated");
-        if (real_glTranslated) {
-            real_glTranslated(x, y, z);
-        }
-    #endif
+    // Call real function (works for both native and WebGL)
+    if (real_glTranslated) {
+        real_glTranslated(x, y, z);
+    }
 }
 
 #endif // MATRIX_DEBUG
