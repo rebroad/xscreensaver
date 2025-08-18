50378830ed3c0fec4568893fdea2587ab68d818a
R E Broadley <rebroad@users.noreply.github.com>
2025-08-18 08:58:51 +0100

    debug thickness and DL

---
+++ utils/debug.c
@@ -0,0 +1,28 @@
+/* debug.c - Debug logging utilities for xscreensaver hacks
+ *
+ * This provides a common debug logging interface that can be used
+ * by all xscreensaver hacks, both native and web builds.
+ */
+
+#include "debug.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+// Global debug level - can be modified at runtime
+int debug_level = 0;  // Default to no debug output
+
+// Debug logging function
+// level 0: errors (stderr)
+// level 1+: info/debug (stdout)
+void DL(int level, const char* format, ...) {
+  if (level <= debug_level) {
+    va_list args;
+    va_start(args, format);
+    if (level == 0) {
+      vfprintf(stderr, format, args);
+    } else {
+      vfprintf(stdout, format, args);
+    }
+    va_end(args);
+  }
+}
