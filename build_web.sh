6705e055367f0a0da186e258a6ed01534a16ce26
R E Broadley <rebroad@users.noreply.github.com>
2025-08-18 18:12:36 +0100

    web move files into web

--- build_web.sh
+++ build_web.sh
@@ -271,7 +271,7 @@ fi
 
 # Add matrix debugging flag and source file if requested
 if [ "$MATRIX_DEBUG" = true ]; then
-    EMCC_ARGS=(-DMATRIX_DEBUG "$REPO_ROOT/matrix_debug.c" "${EMCC_ARGS[@]}")
+    EMCC_ARGS=(-DMATRIX_DEBUG "$REPO_ROOT/web/matrix_debug.c" "${EMCC_ARGS[@]}")
 fi
 
 # Compile with emscripten using custom HTML template
--- hacks/glx/hextrail_web_main.c
+++ hacks/glx/hextrail_web_main.c
@@ -5,10 +5,10 @@
  */
 
 #include <emscripten.h>
-#include "../xscreensaver_web.c"
+#include "../../web/xscreensaver_web.c"
 
 // Include our web headers (WEB_BUILD already defined by build script)
-#include "../xlockmore_web.h"
+#include "../../web/xlockmore_web.h"
 
 // Now include hextrail.c with our web headers already defined
 #include "hextrail.c"
