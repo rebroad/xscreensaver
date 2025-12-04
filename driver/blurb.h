1077eaf35e404497c1fdc8694c69f7b860a5ac7e
R E Broadley <rebroad@users.noreply.github.com>
2025-12-04 14:41:07 -0500

    simplify logging (use systemd)

--- driver/blurb.h
+++ driver/blurb.h
@@ -4,13 +4,22 @@
 #define __BLURB_H__
 
 #include <stdarg.h>
+#include <stdio.h>
 
 extern const char *progname;
 extern int verbose_p;
 extern const char *blurb (void);
 
-/* Debug logging to file */
-extern void debug_log (const char *format, ...);
+/* Debug logging macro - checks verbose_p and prints to stderr with file and line.
+   Note: systemd/journalctl already adds progname, PID, and timestamp, so we don't use blurb() here. */
+#define debug_log(...) \
+  do { \
+    if (verbose_p) { \
+      fprintf (stderr, "[%s:%d] ", __FILE__, __LINE__); \
+      fprintf (stderr, __VA_ARGS__); \
+      fprintf (stderr, "\n"); \
+    } \
+  } while (0)
 
 #endif /*  __BLURB_H__ */
 
